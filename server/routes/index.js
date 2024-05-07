8cfbf79eb8a1f585337096d717b5fed615793d50
yusikerem <kerem.yky@gmail.com>
2024-05-07 19:22:18 +0300

    prettier format

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,12 +9,11 @@ const {
 
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-  res.end('Welcome...')
-})
+router.get("/", (req, res) => {
+  res.end("Welcome...");
+});
 router.get("/todos", getTodos);
 router.post("/todos", createValidator, createTodo);
 router.delete("/todos/:id", deleteTodo);
 router.put("/todos/:id", updateTodo);
-//
 module.exports = router;
